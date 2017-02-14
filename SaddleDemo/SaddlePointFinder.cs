58f4dfa6f4ac216df79b8c49a3a23646b31ff968
david <david@MIDWAY>
2017-02-14 12:34:13 +0100

    Add project files.

---
+++ SaddleDemo/SaddlePointFinder.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace SaddleDemo
+{
+    public class SaddlePointFinder
+    {
+        public string GetSaddlePoints(int[,] input)
+        {
+            if (IsLargest(input.Cast<int>().Take(5).ToArray(), input[0, 0]) &&
+                IsSmallest(GetColumn(input, 0), input[0, 0]))
+                return "0,0";
+            return "No saddle points found.";
+        }
+
+        private bool IsLargest(int[] row, int item)
+        {
+            foreach (int i in row)
+                if (i > item) return false;
+            return true;
+        }
+
+        private bool IsSmallest(int[] column, int item)
+        {
+            foreach (int i in column)
+                if (i < item) return false;
+            return true;
+        }
+
+        private int[] GetColumn(int[,] mx, int columnIndex)
+        {
+            int[] result = new int[mx.GetLength(1)];
+            for (int x = 0; x < result.Length; x++)
+                result[x] = mx[columnIndex, x];
+            return result;
+        }
+    }
+}
